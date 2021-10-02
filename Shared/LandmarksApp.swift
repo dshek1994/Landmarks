a5532fe12a151142b54be02964dbc5f9b4aa685a
Daniel S <dshekari@icloud.com>
2021-10-02 17:37:38 -0500

    Created the skeleton code for the Landmark project

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Daniel Shekari on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
